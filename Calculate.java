1cde24d5f7d764338e8a7d53439db1a43a0e58d0
Daniyar771992 <daniyar771992@mail.ru>
2016-11-30 20:01:05 +0800

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -2,7 +2,7 @@ public class Calculate {
         
         public static void main(String[] arg) {
                 System.out.println("Calculate...");
-                int first = int.valueOf(arg[3]);  
+                int first = int.valueOf(arg[0]);  
                 int second = int.valueOf(arg[1]);  
                 int third = int.valueOf(arg[2]);  
                 int div = first + second + third;
