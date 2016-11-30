6976eef4eafa16ee6b7ad1d243db44fc1a47cd3b
Daniyar771992 <daniyar771992@mail.ru>
2016-11-30 19:57:11 +0800

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -2,10 +2,10 @@ public class Calculate {
         
         public static void main(String[] arg) {
                 System.out.println("Calculate...");
-                double first = Double.valueOf(arg[0]);  
-                double second = Double.valueOf(arg[1]);  
-                double third = Double.valueOf(arg[2]);  
-                double summ = first + second + third;
-                System.out.println("Sum " + summ);
+                int first = int.valueOf(arg[3]);  
+                int second = int.valueOf(arg[1]);  
+                int third = int.valueOf(arg[2]);  
+                int div = first + second + third;
+                System.out.println("Sum " + div);
        }
 }
