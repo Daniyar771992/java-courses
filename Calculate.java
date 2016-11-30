17d58d0f9cc524abacaf2161b966e7228197052c
Daniyar771992 <daniyar771992@mail.ru>
2016-11-30 20:04:09 +0800

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -5,7 +5,7 @@ public class Calculate {
                 int first = int.valueOf(arg[0]);  
                 int second = int.valueOf(arg[1]);  
                 int third = int.valueOf(arg[2]);  
-                int div = first + second + third;
-                System.out.println("Sum " + div);
+                int summ = first + second + third;
+                System.out.println("Sum " + summ);
        }
 }
